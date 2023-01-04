ed0294d784a15bc2ab5f4caa95c30a1a6ad9b2c1
beabaumgartner <if21b223@technikum-wien.at>
2023-01-04 05:58:32 +0100

    Packages fertig eingefügt

--- src/main/java/at/fhtw/mtcgapp/service/packages/PackageController.java
+++ src/main/java/at/fhtw/mtcgapp/service/packages/PackageController.java
@@ -2,48 +2,33 @@ package at.fhtw.mtcgapp.service.packages;
 
 import at.fhtw.httpserver.http.ContentType;
 import at.fhtw.httpserver.http.HttpStatus;
-import at.fhtw.httpserver.server.HeaderMap;
 import at.fhtw.httpserver.server.Request;
 import at.fhtw.httpserver.server.Response;
 import at.fhtw.mtcgapp.dal.UnitOfWork;
 import at.fhtw.mtcgapp.controller.Controller;
+import at.fhtw.mtcgapp.dal.repository.PackageRepository;
 import at.fhtw.mtcgapp.dal.repository.SessionRepository;
-import at.fhtw.mtcgapp.exception.DataAccessException;
-import at.fhtw.mtcgapp.exception.DataUpdateException;
-import at.fhtw.mtcgapp.exception.InvalidLoginDataException;
-import at.fhtw.mtcgapp.model.UserCredentials;
+import at.fhtw.mtcgapp.exception.*;
+import at.fhtw.mtcgapp.model.Card;
 import com.fasterxml.jackson.core.JsonProcessingException;
 
 
 public class PackageController extends Controller{
-    public Response loginUser(Request request) {
+    public Response createPackage(Request request) {
         UnitOfWork unitOfWork = new UnitOfWork();
 
         try (unitOfWork) {
-            HeaderMap headermap = request.getHeaderMap();
-            System.out.println("headermap: " + headermap);
-            System.out.println("urlContent: " + request.getUrlContent());
-            System.out.println("Method: " + request.getMethod());
-            System.out.println("pathname: " + request.getPathname());
-            System.out.println("params: " + request.getParams());
-            System.out.println("body: " + request.getBody());
+            new SessionRepository(unitOfWork).checkIfTokenIsAdmin(request);
 
-            UserCredentials user = this.getObjectMapper().readValue(request.getBody(), UserCredentials.class);
-            Integer user_id = new SessionRepository(unitOfWork).getUserIdByLoginData(user);
-            unitOfWork.commitTransaction();
+            Card cards[] = this.getObjectMapper().readValue(request.getBody(), Card[].class);
+            new PackageRepository(unitOfWork).createPackage(cards);
 
-            if(user_id == null)
-            {
-                throw new InvalidLoginDataException("Invalid login data");
-            }
-
-            new SessionRepository(unitOfWork).loginUser(user.getUsername(), user_id);
             unitOfWork.commitTransaction();
 
             return  new Response(
-                    HttpStatus.OK,
+                    HttpStatus.CREATED,
                     ContentType.PLAIN_TEXT,
-                    "User login successful"
+                    "Package and cards successfully created"
             );
         }
         catch (JsonProcessingException exception) {
@@ -60,7 +45,93 @@ public class PackageController extends Controller{
             return new Response(
                     HttpStatus.UNAUTHORIZED,
                     ContentType.PLAIN_TEXT,
-                    "Invalid username/password provided"
+                    "Authentication information is missing or invalid"
+            );
+        }
+        catch (ConstraintViolationException e)
+        {
+            unitOfWork.rollbackTransaction();
+            return new Response(
+                    HttpStatus.CONFLICT,
+                    ContentType.PLAIN_TEXT,
+                    "At least one card in the packages already exists"
+            );
+        }
+        catch (AccessRightsTooLowException e)
+        {
+            unitOfWork.rollbackTransaction();
+            return new Response(
+                    HttpStatus.CONFLICT,
+                    ContentType.PLAIN_TEXT,
+                    "Provided user is not \"admin\""
+            );
+        }
+        catch (DataAccessException e)
+        {
+            unitOfWork.rollbackTransaction();
+            e.printStackTrace();
+            return new Response(
+                    HttpStatus.CONFLICT,
+                    ContentType.PLAIN_TEXT,
+                    "Database Server Error"
+            );
+        }
+        catch (Exception e)
+        {
+            unitOfWork.rollbackTransaction();
+            return new Response(
+                    HttpStatus.INTERNAL_SERVER_ERROR,
+                    ContentType.JSON,
+                    "Internal Server Error"
+            );
+        }
+    }
+
+    public Response acquireCardPackage(Request request) {
+        UnitOfWork unitOfWork = new UnitOfWork();
+
+        try (unitOfWork) {
+            new SessionRepository(unitOfWork).checkIfTokenIsValid(request);
+
+            int package_id = new PackageRepository(unitOfWork).choosePackage();
+            int user_id = new SessionRepository(unitOfWork).getUserIdByToken(request);
+
+            new SessionRepository(unitOfWork).checkIfTokenIsValid(request);
+            new PackageRepository(unitOfWork).acquireCardPackage(package_id, user_id);
+            new PackageRepository(unitOfWork).UpdateCoinsByUserId(user_id);
+
+            unitOfWork.commitTransaction();
+
+            return  new Response(
+                    HttpStatus.CREATED,
+                    ContentType.PLAIN_TEXT,
+                    "A package has been successfully bought"
+            );
+        }
+        catch (JsonProcessingException exception) {
+            unitOfWork.rollbackTransaction();
+            return new Response(
+                    HttpStatus.INTERNAL_SERVER_ERROR,
+                    ContentType.PLAIN_TEXT,
+                    "Internal Server Error"
+            );
+        }
+        catch (InvalidLoginDataException e)
+        {
+            unitOfWork.rollbackTransaction();
+            return new Response(
+                    HttpStatus.UNAUTHORIZED,
+                    ContentType.PLAIN_TEXT,
+                    "Authentication information is missing or invalid"
+            );
+        }
+        catch (NoDataException e)
+        {
+            unitOfWork.rollbackTransaction();
+            return new Response(
+                    HttpStatus.CONFLICT,
+                    ContentType.PLAIN_TEXT,
+                    "No card package available for buying"
             );
         }
         catch (DataUpdateException e)
@@ -69,7 +140,16 @@ public class PackageController extends Controller{
             return new Response(
                     HttpStatus.CONFLICT,
                     ContentType.PLAIN_TEXT,
-                    "Update the token could not be executed."
+                    "Update data was not successfully"
+            );
+        }
+        catch (NotEnoughItemsException e)
+        {
+            unitOfWork.rollbackTransaction();
+            return new Response(
+                    HttpStatus.CONFLICT,
+                    ContentType.PLAIN_TEXT,
+                    "Not enough money for buying a card package"
             );
         }
         catch (DataAccessException e)
--- src/main/java/at/fhtw/mtcgapp/service/packages/PackageService.java
+++ src/main/java/at/fhtw/mtcgapp/service/packages/PackageService.java
@@ -6,7 +6,6 @@ import at.fhtw.httpserver.http.Method;
 import at.fhtw.httpserver.server.Request;
 import at.fhtw.httpserver.server.Response;
 import at.fhtw.httpserver.server.Service;
-import at.fhtw.mtcgapp.service.session.SessionController;
 
 public class PackageService implements Service {
     private final PackageController packageController;
@@ -17,8 +16,13 @@ public class PackageService implements Service {
     }
     @Override
     public Response handleRequest(Request request) {
-        if (request.getMethod() == Method.POST) {
-            //return this.packageController.loginUser(request);
+        if (request.getMethod() == Method.POST &&
+                request.getPathParts().size() > 1 &&
+                request.getPathParts().get(1).equals("packages")) {
+            return this.packageController.acquireCardPackage(request);
+        }
+        else if (request.getMethod() == Method.POST) {
+            return this.packageController.createPackage(request);
         }
 
         return new Response(
